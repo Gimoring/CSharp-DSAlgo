6dfe77cd160b3764235b247430832d5b21d0ce29
Gimoring <junpil618@gmail.com>
2022-07-06 17:53:46 +0900

    heap tree priority queue

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -123,15 +123,95 @@ namespace Exercise
         }
     }
     #endregion
-    
+
+    #region Tree
     class TreeNode<T>
     {
         public T Data { get; set; }
         public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
 
     }
+    #endregion
+
+    class PriorityQueue
+    {
+        List<int> _heap = new List<int>();
+        public void Push(int data)
+        {
+            // 힙의 맨 끝에 새로운 데이터를 삽입한다.
+            _heap.Add(data);
+
+            int now = _heap.Count - 1; // 시작 위치
+            // now 부터 도장깨기를 시작
+            while (now > 0)
+            {
+                // 도장깨기를 시도
+                int next = (now - 1) / 2; // 나의 부모의 인덱스
+                if (_heap[now] < _heap[next]) // 만약 내 부모보다 내가 작다면 
+                    break; // 실패. 위로 못 올라감.
+
+                // 두 값을 교체한다.
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // 교체를 했으면 검사 위치를 이동한다
+                now = next;
+            }
+
+        }
+
+        public int Pop()
+        {
+            // 반환할 데이터를 따로 저장
+            int ret = _heap[0];
+
+            // 마지막 데이터를 루트로 이동한다.
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            // 역으로 내려가는 도장깨기 시작
+            int now = 0;
+            while(true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now; // 어디로 갈지 모르겠지만 현재 위치로 설정.
+                // 왼쪽값이 현재값보다 크면 왼쪽 이동
+                if (left <= lastIndex && _heap[next] < _heap[left]) // (left <= lastIndex) 범위에서 안 벗어나면 && ... 
+                    next = left;
+                // 오른쪽값이 현재(왼쪽 이동 포함)값보다 크면 오른쪽으로 이동
+                if (right <= lastIndex && _heap[next] < _heap[right]) // (right <= lastIndex) 범위에서 안 벗어나면 && ... 
+                    next = right;
+
+                // 왼쪽 오른쪽 모두 현재값보다 작으면 종료
+                if (next == now)
+                    break;
+
+                // 두 값을 교체한다
+                int temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+                // 검사 위치를 이동한다
+                now = next;
+
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
+    }
+
     class Program
     {
+        #region TreeMethods
         static TreeNode<string> MakeTree()
         {
             TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
@@ -181,7 +261,7 @@ namespace Exercise
 
             return height;
         }
-
+        #endregion
         static void Main(string[] args)
         {
             // 그래프를 순회하는 방법
@@ -189,12 +269,25 @@ namespace Exercise
             // 2. BFS (Breadth First Search 너비 우선 탐색) -> 주변 있는거 보고 감
             // Graph graph = new Graph();
             // graph.Dijikstra(0);
-            TreeNode<string> root = MakeTree();
-            // PrintTree(root);
-            Console.WriteLine(GetHeight(root));
 
-            GetHeight(root);
-            
+            // 트리 만들고 높이 구하기
+            //TreeNode<string> root = MakeTree();
+            //// PrintTree(root);
+            //Console.WriteLine(GetHeight(root));
+
+            //GetHeight(root);
+
+            PriorityQueue q = new PriorityQueue();
+            q.Push(20);
+            q.Push(10);
+            q.Push(30);
+            q.Push(90);
+            q.Push(40);
+
+            while (q.Count() > 0)
+            {
+                Console.WriteLine(q.Pop());
+            }
 
         }
     }
